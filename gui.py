ca4e80665b3fe2fc65190ae665bae0597da61acd
Wilson Wooten <wilwooten4@gmail.com>
2020-11-06 20:56:35 -0500

    Pushing gui branch.

--- gui.py
+++ gui.py
@@ -1,6 +1,12 @@
 from tkinter import *
 
-root = Tk()
-root.title('Planner')
+from tkinter import messagebox
 
+root = Tk()
+root.geometry("900x600")
+def helloCallBack():
+   msg = messagebox.showinfo( "Hello Python", "Hello World")
+
+B = Button(root, text = "Hello", command = helloCallBack)
+B.place(x=450, y=300)
 root.mainloop()
