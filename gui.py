332c1d3fed9e7cc11b98fe7961f9b5e41786f0af
Jake Wooten <jakewooten6@gmail.com>
2020-11-12 23:56:08 -0500

    rough early implementation with panes

--- gui.py
+++ gui.py
@@ -1,12 +1,49 @@
 from tkinter import *
+#look into tkcalendar
 
-from tkinter import messagebox
+m1 = PanedWindow(height=550, width=1000)
+m1.pack(fill=BOTH, expand=1)
 
-root = Tk()
-root.geometry("900x600")
-def helloCallBack():
-   msg = messagebox.showinfo( "Hello Python", "Hello World")
+# left = Label(m1, text="left pane")
+# m1.add(left)
+lb = Listbox(m1, selectmode=SINGLE)
+lb.insert(1, "Python")
+lb.insert(2, "Perl")
+lb.insert(3, "C")
+lb.insert(4, "PHP")
+lb.insert(5, "JSP")
+lb.insert(6, "Ruby")
 
-B = Button(root, text = "Hello", command = helloCallBack)
-B.place(x=450, y=300)
-root.mainloop()
+m1.add(lb)
+
+m2 = PanedWindow(m1, orient=VERTICAL)
+m1.add(m2)
+
+top = Label(m2, text="top pane")
+m2.add(top)
+
+def remove_list():
+   lb.delete(lb.curselection())
+def add_list():
+   if e.get() != "":
+      lb.insert(END, e.get())
+      e.delete(0, END)
+   add_button.config(bg="pink")
+   print("hello")
+
+e = Entry(m2)
+m2.add(e)
+
+add_button = Button(m2, text="Add to List", bg="#ffffff", command=add_list)
+m2.add(add_button)
+
+remove_button = Button(m2, text="Delete From List", bg="red", command=remove_list)
+m2.add(remove_button)
+
+bottom = Label(m2, text="bottom pane")
+m2.add(bottom)
+
+
+
+
+mainloop()
