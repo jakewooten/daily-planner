f1cba4bc5158e245e05c306f7c66f093cb28d06f
Jake Wooten <jakewooten6@gmail.com>
2020-11-13 20:10:10 -0500

    added calendar widget

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from tkinter import *
-#look into tkcalendar
+from tkcalendar import *
+from datetime import *
+
 
 m1 = PanedWindow(height=550, width=1000)
 m1.pack(fill=BOTH, expand=1)
@@ -29,21 +31,20 @@ def add_list():
       lb.insert(END, e.get())
       e.delete(0, END)
    add_button.config(bg="pink")
-   print("hello")
 
 e = Entry(m2)
 m2.add(e)
 
-add_button = Button(m2, text="Add to List", bg="#ffffff", command=add_list)
+add_button = Button(m2, text="Add to List", command=add_list)
 m2.add(add_button)
 
-remove_button = Button(m2, text="Delete From List", bg="red", command=remove_list)
+remove_button = Button(m2, text="Delete From List", command=remove_list)
 m2.add(remove_button)
 
+cal = Calendar(m2,selectmode="day")
 bottom = Label(m2, text="bottom pane")
 m2.add(bottom)
-
-
+m2.add(cal)
 
 
 mainloop()
